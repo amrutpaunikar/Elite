f4a211f189d6b10de3f3407f95dd36643fc4cc08
amrutpaunikar <amrut0503003@gmail.com>
2025-12-02 16:56:31 +0530

    appended Security config and added Logout Also

---
+++ src/main/java/com/controller/LogoutController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+    @GetMapping("/logout-google")
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+    
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+    
+        // Redirect to Google logout page, then back to your home
+        response.sendRedirect("https://accounts.google.com/Logout?continue=https://elite-production-5537.up.railway.app/googlelogin");
+    }
+    
+}
