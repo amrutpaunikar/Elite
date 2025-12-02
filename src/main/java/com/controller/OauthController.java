34563e2bc50e3ff43f71742f1fa99904ae0b5f5a
amrutpaunikar <amrut0503003@gmail.com>
2025-12-02 15:25:36 +0530

    changed to Controller from RestController in OauthController

--- src/main/java/com/controller/OauthController.java
+++ src/main/java/com/controller/OauthController.java
@@ -1,28 +1,17 @@
 package com.controller;
-import java.io.IOException;
-import java.security.Principal;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 public class OauthController {
     
-    @RequestMapping("/user")
-    public Principal user(Principal user) {
-        return user;
-    }
+    
 
     @GetMapping("/googlelogin")
     public String googleLogin() {
         return "redirect:/oauth2/authorization/google";
     }
 
-    @GetMapping("/home")
-public String home() {
-    return "home";
-}
 }
