c8040c5f86ae0d573d19dab1dc484631f1b5e79a
amrutpaunikar <amrut0503003@gmail.com>
2025-12-03 17:22:39 +0530

    home to dashboard

--- src/main/java/com/security/OAuthSuccessHandler.java
+++ src/main/java/com/security/OAuthSuccessHandler.java
@@ -37,7 +37,7 @@ public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         repo.save(new GoogleLoginStats(email, name, picture, new Date()));
     
         // Redirect after login
-        this.setDefaultTargetUrl("/home");
+        this.setDefaultTargetUrl("/dashboard");
     
         super.onAuthenticationSuccess(request, response, authentication);
     }
