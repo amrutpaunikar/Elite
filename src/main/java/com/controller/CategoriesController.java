678a1900f8726dbba8be686726df3b23460bae25
amrutpaunikar <amrut0503003@gmail.com>
2025-12-03 15:33:02 +0530

    UPDATING SecurityConfig

--- src/main/java/com/controller/CategoriesController.java
+++ src/main/java/com/controller/CategoriesController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.model.Ccategories;
+import com.model.Categories;
 import com.repository.CategoriesRepository;
 
 @CrossOrigin(origins = "*")
