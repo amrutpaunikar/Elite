5cc29900e92604107979ea99412fe02aad2f3643
amrutpaunikar <amrut0503003@gmail.com>
2025-12-09 17:14:37 +0530

    change Request body

--- src/main/java/com/controller/ForgetPasswordController.java
+++ src/main/java/com/controller/ForgetPasswordController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -79,8 +80,8 @@ public class ForgetPasswordController {
 
     // 3️⃣ Reset Password
     @PostMapping("/reset-password")
-    public ResponseEntity<?> resetPassword(@RequestParam String email,
-                                           @RequestParam String newPassword) {
+    public ResponseEntity<?> resetPassword(@RequestBody String email,
+                                           @RequestBody String newPassword) {
 
         Optional<User> userOpt = userRepository.findByEmail(email);
         if (userOpt.isEmpty()) {
